45ced8ae1d874c68e947128d0a15e8a8ccf4650b
kevsmir02 <kevsmir02@gmail.com>
2024-08-29 18:05:36 +0800

    Done

--- lab4-oop-basic-incomplete-code/index.php
+++ lab4-oop-basic-incomplete-code/index.php
@@ -1,30 +1,74 @@
 <?php
+class Profile
+{
+    protected $first_name;
+    protected $middle_name;
+    protected $last_name;
+    protected $email;
+    protected $address;
+    protected $favorite_quote;
 
-require "Profile.php";
+    // Constructor
+    public function __construct($last_name, $first_name, $middle_name)
+    {
+        $this->last_name = $last_name;
+        $this->first_name = $first_name;
+        $this->middle_name = $middle_name;
+    }
 
-$profile = new Profile(
-    "Dela Cruz",
-    "Juan",
-    "Dipasisiil"
-);
+    // Method to get complete name
+    public function getCompleteName($format = '{last}, {first} {middle}')
+    {
+        $full_name = $format;
+        $full_name = str_replace('{last}', $this->getLastName(), $full_name);
+        $full_name = str_replace('{first}', $this->getFirstName(), $full_name);
+        $full_name = str_replace('{middle}', $this->getMiddleName(), $full_name);
+        return $full_name;
+    }
 
-$profile->setEmail('juan@delacruz.ph');
-$profile->setAddress('Barangay Mintal, Davao City, Philippines 8000');
+    // Getters and Setters for the properties
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
 
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile: <?php echo $profile->getCompleteName(); ?></title>
-</head>
-<body>
-    <h1><?php echo $profile->getCompleteName(); ?></h1>
-    <h2><?php echo $profile->getEmail();?></h2>
-    <h2><?php echo $profile->getAddress();?></h2>
-    <p>
-        <?php echo $profile->getFavoriteQuote(); ?>
-    </p>
-</body>
-</html>
+    public function getMiddleName()
+    {
+        return $this->middle_name;
+    }
+
+    protected function getLastName()
+    {
+        return $this->last_name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    public function getFavoriteQuote()
+    {
+        return $this->favorite_quote;
+    }
+
+    public function setFavoriteQuote($quote)
+    {
+        $this->favorite_quote = $quote;
+    }
+}
